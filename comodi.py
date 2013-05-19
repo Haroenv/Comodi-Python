e9440b0a1177770134d295f4b2a08e8139d88a74
Andreas Backx <andreas@backx.org>
2013-05-19 19:47:29 +0200

    Added license and more in depth README file.

--- comodi.py
+++ comodi.py
@@ -1,3 +1,8 @@
+print('\033[1mComodi  Copyright (C) 2013  Andreas Backx')
+print('This program comes with ABSOLUTELY NO WARRANTY; for details type \'show w\'.')
+print('This is free software, and you are welcome to redistribute it')
+print('under certain conditions; visit \'http://www.gnu.org/licenses\' for details.\033[0m\n')
+
 from libs import Config
 from libs import Bootstrap as lib_bootstrap
 
