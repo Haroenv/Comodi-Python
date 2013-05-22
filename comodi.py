26522bd3cb7bc69978c80153c1fa1c806df3a9a1
Andreas Backx <andreas@backx.org>
2013-05-22 19:37:31 +0200

    Added and fixed bugs around the play method.

--- comodi.py
+++ comodi.py
@@ -1,7 +1,7 @@
 print('\033[1mComodi  Copyright (C) 2013  Andreas Backx')
-print('This program comes with ABSOLUTELY NO WARRANTY; for details type \'show w\'.')
-print('This is free software, and you are welcome to redistribute it')
-print('under certain conditions; visit \'http://www.gnu.org/licenses\' for details.\033[0m\n')
+print('This program comes with ABSOLUTELY NO WARRANTY.')
+print('This is free software, and you are welcome to redistribute it under')
+print('certain conditions; visit \'http://www.gnu.org/licenses\' for details.\033[0m\n')
 
 from libs import Config
 from libs import Bootstrap as lib_bootstrap
